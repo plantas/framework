ef2d8d11102934a85323b624cc55d95eb21edad4
Igor Plantas <igor.plantas@gmail.com>
2017-11-22 10:07:59 +0100

    removed MAX_FILE_SIZE

--- FormElement/FileUpload.php
+++ FormElement/FileUpload.php
@@ -2,15 +2,20 @@
 
 class FileUpload extends FormElement {
 
-	const MAX_FILE_SIZE = 'maxFileSize';
+	const MAX_FILE_SIZE = 'maxFileSize'; // do not use
+	const MULTIPLE = 'multiple';
 
 	protected $maxFileSize = 100000;
+	protected $multiple = false;
 
 	public function __construct($params = array()) {
 		parent::__construct($params);
 
 		if (isset($params[self::MAX_FILE_SIZE])) {
 			$this->setMaxFileSize($params[self::MAX_FILE_SIZE]);
+		}
+		if (isset($params[self::MULTIPLE])) {
+			$this->setMultiple($params[self::MULTIPLE]);
 		}
 	}
 
@@ -22,15 +27,24 @@ class FileUpload extends FormElement {
 		return $this->maxFileSize;
 	}
 
+	public function setMultiple($multiple) {
+		$this->multiple = (bool) $multiple;
+	}
+
+	public function getMultiple() {
+		return $this->multiple;
+	}
+
 	public function getHtml() {
 		$ret = '';
+		/*
 		$h = new Hidden(array(
 			FormElement::NAME => 'MAX_FILE_SIZE',
 			FormElement::VALUE => $this->getMaxFileSize()
 		));
 		$ret .= $h->getHtml();
-
-		$ret .= '<input type="file" name="' . $this->getName() . '"';
+		 */
+		$ret .= '<input type="file" name="' . $this->getName() . ($this->multiple ? '[]' : '') . '"';
 		$id = $this->getId();
 		if ($id) {
 			$ret .= ' id="' . $id . '"';
@@ -41,6 +55,9 @@ class FileUpload extends FormElement {
 		}
 		if ($this->getReadOnly()) {
 			$ret .= ' readonly="readonly"';
+		}
+		if ($this->getMultiple()) {
+			$ret .= ' multiple';
 		}
 
 		$ret .= ' />';
