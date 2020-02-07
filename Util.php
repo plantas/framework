ab02fe3ada823f77e99e12b49071179cf13f8f10
Igor Plantas <igor.plantas@gmail.com>
2020-02-07 22:08:25 +0100

    OIB validator

--- Util.php
+++ Util.php
@@ -151,6 +151,35 @@ class Util {
 		return (bool) filter_var($email, FILTER_VALIDATE_EMAIL);
 	}
 	
+	public static function isValidOib($oib) {
+		if ( strlen($oib) == 11 ) {
+			if ( is_numeric($oib) ) {
+
+				$a = 10;
+
+				for ($i = 0; $i < 10; $i++) {
+
+					$a = $a + intval(substr($oib, $i, 1), 10);
+					$a = $a % 10;
+
+					if ( $a == 0 ) { $a = 10; }
+
+					$a *= 2;
+					$a = $a % 11;
+
+				}
+
+				$kontrolni = 11 - $a;
+				if ( $kontrolni == 10 ) { $kontrolni = 0; }
+
+				return $kontrolni == intval(substr($oib, 10, 1), 10);
+			}
+			return false;
+
+		}
+		return false;	
+	}
+
 	public static function titleSlug($in) {
 		$out = self::transliterate($in);
 		$out = preg_replace('/[^a-zA-Z0-9\-\s_]*/','', $out);
