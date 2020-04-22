9a9f60b073b2552a2a192d51096b0c24e58ca816
Igor Plantas <igor.plantas@gmail.com>
2020-04-22 16:38:38 +0200

    dataTables grid renderer

--- Grid/GridRendererDataTableDOM.php
+++ Grid/GridRendererDataTableDOM.php
@@ -2,6 +2,12 @@
 
 class GridRendererDataTableDOM extends GridRendererTable {
 
+	protected $options = [
+		self::PAGER_RPP_OPTIONS => 100000,
+	];
+
+	protected $tableDomId;
+
 	public function getHtml() {
 		File::includeCss('grid/table.css', File::LIB_DIR);
 		$theme = $this->getOption(self::THEME);
@@ -13,26 +19,43 @@ class GridRendererDataTableDOM extends GridRendererTable {
 		}
 		Lang::addDictionary('grid.php');
 
+		$this->tableDomId = 'grid-table-' . uniqid();
+
 		$html = '
 		<div class="grid-container">
 			<div class="grid-toolbar">
 			' . $this->getToolbar() . '
 			</div>
 			<div class="grid-data">
-				<table border="1" class="grid">
+				<table border="1" class="grid" id="' . $this->tableDomId . '">
 				<thead>
 				' . $this->getTableHeader() . '
 				</thead>
 				<tbody>
 				' . 
-				$this->getTableBody() . 
-				$this->getTableSummary() . '
+				$this->getTableBody() . '
 				</tbody>
 				</table>
 			</div>
 		</div>';
 		
 		return $html;
+	}
+
+	protected function getTableHeader() {
+		$html = '<tr>';
+		$cols = $this->getGrid()->getColumns();
+		foreach ($cols as $col) {
+			if ($col instanceof GridColumn && $col->getVisible()) {
+				$html .= '<th class="' . $col->getType() . ' ' . $col->getName() . '">' . $col->getTitle() . '</th>';
+			}
+		}
+		$html .= '</tr>';
+		return $html;
+	}
+
+	public function getTableDomId() {
+		return $this->tableDomId;
 	}
 
 	protected function tabsFactory(array $params) {
